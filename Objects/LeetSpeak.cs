6b9f40c4a5b42944abae4f06a342c3c55a388b61
George Olson and Joel Delight <george+joel@>
2016-07-05 10:47:53 -0700

    convert the letter 'e' into a '3'

--- Objects/LeetSpeak.cs
+++ Objects/LeetSpeak.cs
@@ -4,8 +4,26 @@ using System;
 namespace LeetSpeak.Objects
 {
 
-  public class LeetSpeakTranslator {
-    
+  public class LeetSpeakTranslator
+  {
+    public string LeetSpeakGenerator(string input)
+    {
+      string result = "";
+      char[] inputtedArray = input.ToCharArray();
+      foreach (char character in inputtedArray)
+      {
+        if (character.ToString() == "e")
+        {
+          result = result + "3";
+        }
+        else
+        {
+          result = result + character.ToString();
+        }
+      }
+      // string result = string.Join("", inputtedArray);
+      return result;
+    }
   }
 
 }
--- Tests/LeetSpeakTest.cs
+++ Tests/LeetSpeakTest.cs
@@ -5,6 +5,18 @@ namespace LeetSpeak.Objects
 {
   public class LeetSpeakTest
   {
-    
+    [Fact]
+    public void LeetSpeakGenerator_userInputTree_ReturnTr33()
+    {
+      //Arrange
+      string expectedResult = "Tr33";
+      LeetSpeakTranslator newLeetSpeakTranslator = new LeetSpeakTranslator();
+
+      //Act
+      string result = newLeetSpeakTranslator.LeetSpeakGenerator("Tree");
+
+      //Assert
+      Assert.Equal(result, expectedResult);
+    }
   }
 }
